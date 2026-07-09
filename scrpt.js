9133402b64703077accba0a1fd14e670a70844b2
eduardadesouza26-lgtm <eduarda.de.souza26@escola.pr.gov.br>
2026-07-09 10:56:22 -0300

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -1,1 +1,17 @@
+const botao = document.getElementById("botao");
 
+botao.addEventListener("click", () => {
+    alert("Olá! Você clicou no botão.");
+});
+
+const tema = document.getElementById("tema");
+
+tema.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        tema.textContent = "☀️";
+    }else{
+        tema.textContent = "🌙";
+    }
+});
